63e8c703e537e3eaee41a734c02f0af03a131d96
fs243 <fa.sharafkhani243@gmail.com>
2024-01-02 02:36:09 -0600

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -38,11 +38,53 @@ class modelTrainer:
             "linear regression": LinearRegression(),
             "xgbRegressor": XGBRegressor(),
             "catboost":CatBoostRegressor(verbose=False),
-            "adaBoost":AdaBoostRegressor()
+            'random forest':RandomForestRegressor()
 
             }
+           params={
+                "decision tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                'random forest':{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "gradient boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    # 'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "linear regression":{},
+                "kneighborns":{
+                    'n_neighbors':[5,7,9,11],
+                    # 'weights':['uniform','distance'],
+                    # 'algorithm':['ball_tree','kd_tree','brute']
+                },
+                "xgbRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "catboost":{
+                    'depth': [6,8,10],
+                    # 'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "adaBoost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
            
-           model_report:dict= evaluate_models(X_train=X_train, y_train=y_train,X_test=X_test ,y_test=y_test, models=models)
+           model_report:dict= evaluate_models(X_train=X_train, y_train=y_train,X_test=X_test ,y_test=y_test, models=models,params=params)
            best_model_score= max(sorted(model_report.values()) )
 
            best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
